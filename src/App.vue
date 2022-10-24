eff5926822e3a80cf1c292e08cdeb7943465f5a8
sevalmercan <sevalmercan.sm@gmail.com>
2022-10-24 14:45:44 +0300

    app created and first object moved

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <div ref="modal" class="game-board">
+      <div class=" square" ref="square" :style="{ top: topNumber + 'px', left: leftNumber + 'px' }">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import mixin from './mixin';
+
+export default {
+  mixins: [mixin],
+  name: 'App',
+  data() {
+    return {
+      topNumber: 0,
+      leftNumber: 175,
+    }
+  },
+  created() {
+
+    const that = this;
+    const timer = setInterval(function () {
+      if (that.topNumber >= 332) {
+        clearInterval(timer);
+      } else {
+        that.moveDown()
+      }
+    }, 1000);
+
+  },
+  methods: {
+    moveLeft() {
+      this.leftNumber = this.leftNumber - 5;
+    },
+
+    moveRight() {
+      this.leftNumber = this.leftNumber + 5;
+    },
+    moveDown() {
+      this.topNumber = this.topNumber + 10;
+    },
+  }
+} 
+</script>
+
+<style>
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.game-board {
+
+  border: 2px solid red;
+  width: 350px;
+  height: 350px;
+}
+
+.square {
+  width: 15px;
+  height: 15px;
+  background-color: blue;
+  position: relative;
+  left: calc(50% - 16px);
+
+
+}
+</style>
---
+++ src/mixin.js
@@ -0,0 +1,19 @@
+export default {
+  created() {
+    let that = this;
+    document.addEventListener("keyup", function (event) {
+      const key_code = event.key;
+      switch (key_code) {
+        case "ArrowLeft": //left arrow key
+          that.moveLeft();
+          break;
+        case "ArrowRight": //right arrow key
+          that.moveRight();
+          break;
+        case "ArrowDown": //down arrow key
+          that.moveDown();
+          break;
+      }
+    });
+  },
+};
