6d1b0d784c6db356fce7127a1627371434569141
sevalmercan <sevalmercan.sm@gmail.com>
2022-10-25 10:46:11 +0300

    infinitive loop created

--- src/components/game-board.vue
+++ src/components/game-board.vue
@@ -16,7 +16,7 @@
 import mixin from '../mixin';
 
 const MIDDLE_OF_THE_BOARD = 175
-const END_OF_THE_BOARD = 332
+const END_OF_THE_BOARD = 330
 
 export default {
     mixins: [mixin],
@@ -27,11 +27,18 @@ export default {
     },
     created() {
 
+        // İnitialaze game
+        this.addObjectToArray();
+
         const that = this;
         const timer = setInterval(function () {
             if (that.currentObject?.topNumber >= END_OF_THE_BOARD) {
-                clearInterval(timer);
+                console.log(that.objectArray)
+                that.addObjectToArray();
+
+
             } else if (that.objectArray?.length >= 1) {
+                console.log(that.currentObject?.topNumber)
                 that.moveDown()
             }
         }, 1000);
@@ -46,20 +53,20 @@ export default {
             this.currentObject.leftNumber = this.currentObject.leftNumber + 5;
         },
         moveDown() {
+
             this.currentObject.topNumber = this.currentObject.topNumber + 10;
         },
         addObjectToArray() {
             this.objectArray.push({ id: this.numberOfObject, topNumber: 0, leftNumber: MIDDLE_OF_THE_BOARD })
-
         }
     },
     computed: {
         currentObject() {
-            return this.objectArray[0]
+            return this.objectArray[this.numberOfObject - 1]
         },
         numberOfObject() {
             return this.objectArray.length
-        }
+        },
     }
 }
 </script>
@@ -69,14 +76,15 @@ export default {
 
     border: 2px solid red;
     width: 350px;
-    height: 350px;
+    height: 342px;
+    position: relative;
 }
 
 .square {
     width: 15px;
     height: 15px;
     background-color: blue;
-    position: relative;
+    position: absolute;
     left: calc(50% - 16px);
 
 
